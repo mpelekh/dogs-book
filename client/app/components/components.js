6f042790a2e03e36af11beae127f76b02b465de1
nick.peleh <nick.peleh@gmail.com>
2016-12-03 15:20:27 +0200

    Add to remove folder all unused components

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,6 +1,4 @@
 import angular from 'angular';
-import Home from './home/home';
-import About from './about/about';
 import Dogs from './dogs/dogs';
 import DogProfile from './dog-profile/dog-profile';
 import Users from './users/users';
