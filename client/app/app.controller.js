512a4787124fdd4f9f43b0005f43384da35be42c
nick.peleh <nick.peleh@gmail.com>
2016-12-04 13:22:51 +0200

    Comment app.controller

--- client/app/app.controller.js
+++ client/app/app.controller.js
@@ -2,20 +2,20 @@ class AppController {
     constructor($scope) {
         "ngInject";
 
-        $scope.$on('$viewContentLoaded', function () {
-            //Here your view content is fully loaded !!
-            (function ($) {
-                $(function () {
-                    // Custom Selects
-                    if ($('[data-toggle="select"]').length) {
-                        $('[data-toggle="select"]').select2();
-                    }
-                    // Checkboxes and Radio buttons
-                    $('[data-toggle="checkbox"]').radiocheck();
-                    $('[data-toggle="radio"]').radiocheck();
-                });
-            })(jQuery);
-        });
+        // $scope.$on('$viewContentLoaded', function () {
+        //     //Here your view content is fully loaded !!
+        //     (function ($) {
+        //         $(function () {
+        //             // Custom Selects
+        //             if ($('[data-toggle="select"]').length) {
+        //                 $('[data-toggle="select"]').select2();
+        //             }
+        //             // Checkboxes and Radio buttons
+        //             $('[data-toggle="checkbox"]').radiocheck();
+        //             $('[data-toggle="radio"]').radiocheck();
+        //         });
+        //     })(jQuery);
+        // });
     }
 }
 
