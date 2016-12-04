9cb8eeb70c78634edcee9cee392288df7c17f856
nick.peleh <nick.peleh@gmail.com>
2016-12-04 16:48:58 +0200

    Update dogs

--- client/app/components/dogs/dogs.controller.js
+++ client/app/components/dogs/dogs.controller.js
@@ -1,8 +1,10 @@
 class DogsController {
-  constructor(DogsNetworkService) {
+  constructor(DogsNetworkService, $q) {
     'ngInject';
 
     const self = this;
+
+    self._$q = $q;
 
     self._DogsNetworkService = DogsNetworkService;
 
@@ -17,28 +19,58 @@ class DogsController {
     const getSpecificModelData = self._DogsNetworkService.getSpecificModelData.bind(self._DogsNetworkService);
     const { REGIONS_URL, GENDERS_URL, STATUSES_URL, SIZES_URL } = self._DogsNetworkService;
 
-    getSpecificModelData(REGIONS_URL).then(regions => {
-      self.regions = regions.data;
-      self.filter.region = regions.data[0];
-    });
-    getSpecificModelData(GENDERS_URL).then(genders => {
-      self.genders = genders.data;
-      self.filter.gender = genders.data[0];
-    });
-    getSpecificModelData(STATUSES_URL).then(statuses => self.statuses = statuses.data);
-    getSpecificModelData(SIZES_URL).then(sizes => self.sizes = sizes.data);
+    // getSpecificModelData(REGIONS_URL).then(regions => {
+    //   self.regions = regions.data;
+    //   self.filter.region = regions.data[0];
+    // });
+    // getSpecificModelData(GENDERS_URL).then(genders => {
+    //   self.genders = genders.data;
+    //   self.filter.gender = genders.data[0];
+    // });
+    // getSpecificModelData(STATUSES_URL).then(statuses => self.statuses = statuses.data);
+    // getSpecificModelData(SIZES_URL).then(sizes => self.sizes = sizes.data);
+
+    self._$q.all([
+      getSpecificModelData(REGIONS_URL).then(regions => {
+        self.regions = regions.data;
+        self.filter.region = regions.data[0];
+        return regions.data;
+      }),
+      getSpecificModelData(GENDERS_URL).then(genders => {
+        self.genders = genders.data;
+        self.filter.gender = genders.data[0];
+        return genders.data;
+      }),
+      getSpecificModelData(STATUSES_URL).then(statuses => {
+        self.statuses = statuses.data;
+        return statuses.data;
+      }),
+      getSpecificModelData(SIZES_URL).then(sizes => {
+        self.sizes = sizes.data;
+        return sizes.data;
+      })
+    ])
+      .then(data => {
+        self.filter.region = data[0][0];
+        self.filter.gender = data[1][0];
+        self.filter.statuses = data[2][0];
+        self.filter.sizes = data[3][0];
+      })
+      .then(result => {
+        setTimeout(() => {
+          if ($('[data-toggle="select"]').length) {
+            $('[data-toggle="select"]').select2();
+          }
+          // Checkboxes and Radio buttons
+          $('[data-toggle="checkbox"]').radiocheck();
+          $('[data-toggle="radio"]').radiocheck();
+        }, 0);
+      });
 
     self.getDogs().then(dogs => {
-      self.dogs = dogs;
+      console.log('dogs', dogs);
+      self.dogs = dogs.data;
     });
-
-    //TODO: Remove mock data.
-    //const { statuses, genders, sizes, regions } = self.getMockData();
-    //
-    //self.statuses = statuses;
-    //self.genders = genders;
-    //self.sizes = sizes;
-    //self.regions = regions;
   }
 
   getDogs() {
@@ -48,14 +80,13 @@ class DogsController {
   }
 
   filterDogs(filter) {
-    debugger;
     const self = this;
 
     const originalFilter = Object.assign({}, filter);
     const preparedFilter = {};
 
     Object.keys(originalFilter).forEach(key => {
-      if(typeof originalFilter[key] === 'object') {
+      if (typeof originalFilter[key] === 'object') {
         originalFilter[key] = originalFilter[key].id;
       }
     });
@@ -66,16 +97,38 @@ class DogsController {
       }
     });
 
-    if(Object.keys(preparedFilter).length === 0) return;
+    if (Object.keys(preparedFilter).length === 0) return;
+
+    console.log('--- preparedFilter', preparedFilter);
+
 
     self._DogsNetworkService.getFilteredDogs(preparedFilter)
       .then(data => {
         //TODO: Implement
+        console.log('--- data', data);
+
       },
       error => {
         //TODO: Implement
-      }
-    )
+        console.log('--- error', error);
+
+      });
+  }
+
+  getItemById(id, itemName) {
+    const self = this;
+
+    if(!self[itemName]) return null;
+
+    let result = null;
+
+    try {
+      result = self[itemName].filter(item => item.id = id)[0];
+    } catch (error) {
+      console.log(error);
+    }
+
+    return result;
   }
 }
 
