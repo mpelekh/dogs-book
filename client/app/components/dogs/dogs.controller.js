3902b4108f9bd2078eae0f6ae9ae01f8d5b80f6e
agronom <agronom.pele@gmail.com>
2016-12-04 11:07:18 +0200

    Work with dog filter

--- client/app/components/dogs/dogs.controller.js
+++ client/app/components/dogs/dogs.controller.js
@@ -12,10 +12,28 @@ class DogsController {
   $onInit() {
     const self = this;
 
+    self.filter = {};
+
+    //TODO: uncomment.
+    //const getSpecificModelData = self._DogsNetworkService.getSpecificModelData;
+    //const { REGIONS_URL, GENDERS_URL, STATUSES_URL, SIZES_URL } = self._DogsNetworkService;
+    //
+    //getSpecificModelData(REGIONS_URL).then(regions => self.regions = regions);
+    //getSpecificModelData(GENDERS_URL).then(genders => self.genders = genders);
+    //getSpecificModelData(STATUSES_URL).then(statuses => self.statuses = statuses);
+    //getSpecificModelData(SIZES_URL).then(sizes => self.sizes = sizes);
+
     self.getDogs().then(dogs => {
       //TODO: remove data
       self.dogs = dogs.data;
     });
+
+    //TODO: Remove mock data.
+    const { statuses, genders, sizes } = self.getMockData();
+
+    self.statuses = statuses;
+    self.genders = genders;
+    self.sizes = sizes;
   }
 
   getDogs() {
@@ -28,6 +46,55 @@ class DogsController {
     if(!self.dogs) return;
 
     //TODO: implement.
+  }
+
+  getMockData() {
+    const statuses = [
+      {
+        id: 1,
+        name: 'terminovo'
+      },
+      {
+        id: 2,
+        name: 'shykaje peretrymku'
+      },
+      {
+        id: 3,
+        name: 'shukaje dim'
+      }
+    ];
+
+    const genders = [
+      {
+        id: 1,
+        name: 'male'
+      },
+      {
+        id: 2,
+        name: 'female'
+      }
+    ];
+
+    const sizes = [
+      {
+        id: 1,
+        name: 'small'
+      },
+      {
+        id: 2,
+        name: 'medium'
+      },
+      {
+        id: 3,
+        name: 'big'
+      }
+    ];
+
+    return {
+      statuses,
+      genders,
+      sizes
+    };
   }
 }
 
